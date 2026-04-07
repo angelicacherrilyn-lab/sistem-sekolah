58cc3e0667bdd3725091340ea67b46ade5706a76
cherrilynangelica <cherrilynangelica@gmail.com>
2026-04-07 12:24:37 +0700

    feat: implement submit button and back button in student create page

--- app/views/students/create.php
+++ app/views/students/create.php
@@ -26,19 +26,24 @@
            <form action="" class= "grid grid-cols-2 gap-4">
                 <div class= "space-y-2">
                     <label class= "block font-bold" for="name">Nama</label>
-                    <input class= "w-full px-4 py-2 border rounded-lg" type="text" id="name" placeholder="Masukan Nama" name="Name">
+                    <input class= "w-full px-4 py-2 border rounded-lg" type="text" id="name" placeholder="Masukan Nama" name="name">
                 </div>
                 <div class= "space-y-2">
                     <label class= "block font-bold" for="class">Kelas</label>
-                    <input class= "w-full px-4 py-2 border rounded-lg" type="text" id="name" placeholder="Masukan Kelas" name="Name">
+                    <input class= "w-full px-4 py-2 border rounded-lg" type="text" id="name" placeholder="Masukan Kelas" name="class">
                 </div>
                 <div class= "space-y-2">
                     <label class= "block font-bold" for="NIS">NIS</label>
-                    <input class= "w-full px-4 py-2 border rounded-lg" type="text" id="name" placeholder="Masukan NIS" name="Name">
+                    <input class= "w-full px-4 py-2 border rounded-lg" type="text" id="name" placeholder="Masukan NIS" name="NIS">
                 </div>
                 <div class= "space-y-2">
                     <label class= "block font-bold" for="phone_number">Nomor Telepon</label>
-                    <input class= "w-full px-4 py-2 border rounded-lg" type="text" id="name" placeholder="Masukan Nomor Telepon" name="Name">
+                    <input class= "w-full px-4 py-2 border rounded-lg" type="text" id="name" placeholder="Masukan Nomor Telepon" name="phone_number">
+                </div>
+
+                <div class="flex justify-end col-span-2">
+                    <a href="/students" class="py-2 px-4 bg-gray-100 rounded-lg">Kembali</a>
+                    <button type="submit" class="py-2 px-4 bg-blue-500 rounded-lg text-white">Simpan</button>
                 </div>
            </form>
         </div>
